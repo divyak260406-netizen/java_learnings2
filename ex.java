4f564f29afb0e9be78d271dab53f3eea89e32ebb
unknown <divyak260406@gmail.com>
2026-02-10 15:35:05 +0530

    completed upto methods

---
+++ ex.java
@@ -0,0 +1,39 @@
+import java.lang.System;
+class Exercise{
+    public static void main(String[] args) {
+        String name = "Divya k";
+        int age = 20;
+        float height = 5.2f;
+        double weight = 55.2;
+        System.out.println("Name :" +name);
+        System.out.println("Age :" +age);
+        System.out.println("Height :" +height);
+        System.out.println("Weight :" +weight);
+    }
+}
+
+
+class Exercise1{
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+        int sum = a+b;
+        int difference = a-b;
+        int product = a*b;
+        double division = (double)a/b;
+        System.out.println("sum: "+sum);
+        System.out.println("difference: "+difference);
+        System.out.println("product: "+product);
+        System.out.println("division: "+division);
+    }
+}
+
+class Exercise2{
+    public static void main(String[] args) {
+        String city="Tumkur";
+        int pin=572106;
+        System.out.println("city: "+city);
+        System.out.println("pin: "+pin);
+    }
+}
+
